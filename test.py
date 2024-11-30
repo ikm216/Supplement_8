8136ba326e3e75240565786febe98e10abd3fab3
ikm216 <ikmclassof22@gmail.com>
2024-11-30 17:15:29 -0500

    doc: added documentation for the feature http_get_request_url function

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 import requests
 
 def http_get_request_url(url):
+    """
+    Sends an HTTP GET request to the specified URL and returns the status code and response data.
+
+    Args:
+        url: The URL to send the GET request to.
+
+    Returns:
+        A tuple of the HTTP status code of the response and the JSON-parsed response data if the status code is 200.
+
+    Raises:
+        Exception: If the HTTP status code is not 200, an exception is raised
+    """
     response = requests.get(url)
     if response.status_code == 200:
         pokemon_data = response.json()
