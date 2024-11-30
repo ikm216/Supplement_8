7b5d2a9bfbca56ca79b695aea5eaac64cc22b725
ikm216 <ikmclassof22@gmail.com>
2024-11-30 17:12:06 -0500

    feat: added feature for getting the get request from a url

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import requests
 
+def http_get_request_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return response.status_code, pokemon_data
+    else:
+        raise Exception(f"Failed to get: {response.status}")
+
 def test_should_return_get_request():
     url = "https://pokeapi.co/api/v2/"
     response = http_get_request_url(url)
